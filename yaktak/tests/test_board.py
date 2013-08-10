1b2fa9b236a3eece8aac4948df7bac20026faef1
Ryan Seto <mr.werewolf@gmail.com>
2013-08-09 21:07:06 -0400

    Test moving off the board and bogus moves.

--- yaktak/tests/test_board.py
+++ yaktak/tests/test_board.py
@@ -34,3 +34,28 @@ class TestBoard(TestCase):
                 str.format("Got {0}. Expected 1. Should be X's turn",
                     board.turn()))
 
+    def test_bogus_move(self):
+        """Make sure we raise an exception if we make a bogus move."""
+
+        board = Board()
+
+        with self.assertRaises(ValueError):
+            board._move(0, 0, 0)
+        with self.assertRaises(ValueError):
+            board._move(0, 0, 2)
+        with self.assertRaises(ValueError):
+            board._move(0, 0, -2)
+
+    def test_move_off_board(self):
+        """Make sure we can't make a move that's off the board."""
+
+        board = Board()
+
+        with self.assertRaises(IndexError):
+            board.xmove(-1, 0)
+        with self.assertRaises(IndexError):
+            board.xmove(0, -1)
+        with self.assertRaises(IndexError):
+            board.xmove(3, 0)
+        with self.assertRaises(IndexError):
+            board.xmove(0, 3)

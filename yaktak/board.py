b3f34c9ab12d125ad1e62570a12c90eab50a45af
Ryan Seto <mr.werewolf@gmail.com>
2013-08-09 20:37:26 -0400

    Let's add some tests.

--- yaktak/board.py
+++ yaktak/board.py
@@ -7,19 +7,19 @@ class Board:
         # 0 = the space is not filled.
         # 1 = x
         # -1 = o
-        self.board = [[0,0,0],
+        self._board = [[0,0,0],
                       [0,0,0],
                       [0,0,0]]
 
     def _move(self, x, y, num):
         if num > 1 or num < -1 or num == 0:
-            raise ValueError(str.format('num can only be 1 or -1. Got: {0}', num))
+            raise ValueError(str.format("num can only be 1 or -1. Got: {0}", num))
 
         if x > 2 or y > 2 or x < 0 or y < 0:
             raise IndexError(
-                str.format('The x and y coordinates can be from (0, 0) to (2,2). Got: ({0},{1})', x, y))
+                str.format("The x and y coordinates can be from (0, 0) to (2,2). Got: ({0},{1})", x, y))
 
-        self.board[y][x] = num
+        self._board[y][x] = num
 
     def xmove(self, x, y):
         if self.turn() != 1:
@@ -49,9 +49,9 @@ class Board:
 
         for i in range(0, 3):
             for j in range (0, 3):
-                if self.board[i][j] == 1:
+                if self._board[i][j] == 1:
                     xmoves += 1
-                elif self.board[i][j] == -1:
+                elif self._board[i][j] == -1:
                     omoves += 1
 
         if xmoves and omoves == 0:
@@ -73,8 +73,8 @@ class Board:
             rowsum = 0
             colsum = 0
             for j in range(0, 3):
-                rowsum += self.board[i][j]
-                colsum += self.board[j][i]
+                rowsum += self._board[i][j]
+                colsum += self._board[j][i]
 
             if rowsum == 3 or colsum == 3:
                 return 1
@@ -82,8 +82,8 @@ class Board:
                 return -1
 
         # Check for diagonal cases
-        diagsum1 = self.board[0][0] + self.board[1][1] + self.board[2][2]
-        diagsum2 = self.board[0][2] + self.board[1][1] + self.board[2][0]
+        diagsum1 = self._board[0][0] + self._board[1][1] + self._board[2][2]
+        diagsum2 = self._board[0][2] + self._board[1][1] + self._board[2][0]
 
         if diagsum1 == 3 or diagsum2 == 3:
             return 1
@@ -95,20 +95,20 @@ class Board:
 
     def __repr__(self):
         return str.format(
-'''
+"""
 {6} | {7} | {8}
 ---------
 {3} | {4} | {5}
 ---------
 {0} | {1} | {2}
-''',
-            self.getSymbol(self.board[0][0]),
-            self.getSymbol(self.board[0][1]),
-            self.getSymbol(self.board[0][2]),
-            self.getSymbol(self.board[1][0]),
-            self.getSymbol(self.board[1][1]),
-            self.getSymbol(self.board[1][2]),
-            self.getSymbol(self.board[2][0]),
-            self.getSymbol(self.board[2][1]),
-            self.getSymbol(self.board[2][2]))
+""",
+            self.getSymbol(self._board[0][0]),
+            self.getSymbol(self._board[0][1]),
+            self.getSymbol(self._board[0][2]),
+            self.getSymbol(self._board[1][0]),
+            self.getSymbol(self._board[1][1]),
+            self.getSymbol(self._board[1][2]),
+            self.getSymbol(self._board[2][0]),
+            self.getSymbol(self._board[2][1]),
+            self.getSymbol(self._board[2][2]))
 
--- yaktak/tests/test_board.py
+++ yaktak/tests/test_board.py
@@ -2,6 +2,35 @@ from unittest import TestCase
 
 from yaktak.board import Board
 
-class BoardTests(TestCase):
-    def test_create(self):
+class TestBoard(TestCase):
+    def test_move(self):
+        """Make sure X and O can make moves."""
         board = Board()
+        board.xmove(0,0)
+        board.omove(1,1)
+        self.assertEqual(board._board[0][0], 1, "X did not get on the board.")
+        self.assertEqual(board._board[1][1], -1, "O did not get on the board.")
+
+    def test_position(self):
+        """Make sure we get on the right positions."""
+        board = Board()
+        board.xmove(2,1)
+        board.omove(0,1)
+        self.assertEqual(board._board[1][2], 1, "X in the wrong position.")
+        self.assertEqual(board._board[1][0], -1, "O in the wrong position.")
+
+    def test_whos_turn(self):
+        """Check who's turn is it."""
+        board = Board()
+        self.assertEqual(board.turn(), 1,
+                str.format("Got {0}. Expected 1. Should be X's turn",
+                    board.turn()))
+        board.xmove(0,0)
+        self.assertEqual(board.turn(), -1,
+                str.format("Got {0}. Expected -1. Should be O's turn",
+                    board.turn()))
+        board.omove(1,1)
+        self.assertEqual(board.turn(), 1,
+                str.format("Got {0}. Expected 1. Should be X's turn",
+                    board.turn()))
+

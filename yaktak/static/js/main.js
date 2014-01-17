b538685731f3f03f8a7252f71ddeee9e4a82b7dd
Ryan Seto <ryanseto@yak.net>
2014-01-17 10:31:14 -0500

    The player can now make a move.  UI gets disabled while we wait on response.  Winner detection and display.

--- yaktak/static/js/main.js
+++ yaktak/static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var boardFields = [
+    var winner = 0,
+        boardFields = [
             [$('#field-20'), $('#field-21'), $('#field-22')],
             [$('#field-10'), $('#field-11'), $('#field-12')],
             [$('#field-00'), $('#field-01'), $('#field-02')]
@@ -44,32 +45,81 @@ $(document).ready(function() {
                 }
             }
         },
+        // Who's turn is it?  Returns 1 or -1.
+        turn = function() {
+            var grid = getGrid(), moves = 0, x, y;
+            for (y = 0; y < 3; y++) {
+                for (x = 0; x < 3; x++) {
+                    if (grid[y][x] != 0) {
+                        moves++;
+                    }
+                }
+            }
+
+            if (moves % 2 == 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        },
         // Resets the board.
         reset = function() {
             setGrid([[0,0,0],[0,0,0],[0,0,0]]);
+            winner = 0;
+            $('#msg').text('');
         },
         // Allows the player to skip a turn.
         skip = function() {
             aiMove();
         },
+        disableUI = function() {
+            $('button').attr('disabled', 'disabled');
+        },
+        enableUI = function() {
+            $('button').removeAttr('disabled');
+        },
         // Request a move from the AI.
         aiMove = function() {
+            // Make sure the user can't use the interface while we wait for
+            // a response.
+            disableUI();
+
             // Handle the response from the AI.
             var success = function(data) {
+                winner = data.winner;
                 setGrid(data.grid);
+                if (winner != 0) {
+                    if (winner > 0) {
+                        $('#msg').text("Player X wins!");
+                    } else {
+                        $('#msg').text("Player O wins!");
+                    }
+                }
             };
+            // Make sure we re-enable the UI.
+            var complete = function() {
+                enableUI();
+            }
             $.ajax({
                 url: '/wopr/move',
                 type: 'POST',
                 data: JSON.stringify(getGrid()),
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'json',
-                success: success
+                success: success,
+                complete: complete
             });
         },
         // Handle button mashing, I mean clicking.
         buttonClickHandler = function() {
-            aiMove();
+            if (winner == 0) {
+                if (turn() > 0) {
+                    $(this).addClass('x');
+                } else {
+                    $(this).addClass('o');
+                }
+                aiMove();
+            }
         };
 
     $('#board button').click(buttonClickHandler);
